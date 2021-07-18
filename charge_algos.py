ea84a374479f7104a6eeb57647c8355d05776210
Shubham Chodankar <31985826+ShubhamC08@users.noreply.github.com>
2021-07-18 09:35:23 +0530

    Update charge_algos.py

--- charge_algos.py
+++ charge_algos.py
@@ -295,7 +295,7 @@ def Emergency_Stop_Routine(spi):
        # led.on()
 
 def Configuration(AH,Make):
-    Config=[]
+    Config=[0,0,0,0,0]
     if AH == '0':
         if Make == '0':
             #Configuration for Jumbo 75AH
